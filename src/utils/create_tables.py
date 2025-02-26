9bb1da50618f50dc704155e11c1aec43840d0177
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-02-26 22:22:25 +0100

    Add student table query

--- src/utils/create_tables.py
+++ src/utils/create_tables.py
@@ -12,5 +12,18 @@ if __name__ == "__main__":
     )
 
 
+    with conn, conn.cursor() as cur:
+        create_student_table_query=sql.SQL(
+            """
+            DROP TABLE IF EXISTS student;
+            CREATE TABLE student (
+                student_id SERIAL PRIMARY KEY,
+                name VARCHAR(128),
+                surname VARCHAR(128),
+                grade INT
+            );
+            """
+        )
+        cur.execute(create_student_table_query)
 
     conn.close()
