286d52a8ac736351e81742a5e5fd27d0417f3d84
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-02-26 22:18:51 +0100

    fix typo

--- src/utils/create_tables.py
+++ src/utils/create_tables.py
@@ -1,10 +1,16 @@
 import psycopg2
 
-conn=psycopg2.connect(
-    host="localhost",
-    databese="postgres",
-    user="postgres",
-    password="password"
-)
+from psycopg2 import sql
 
-conn.close()
+
+if __name__ == "__main__":
+    conn=psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="password"
+    )
+
+
+
+    conn.close()
