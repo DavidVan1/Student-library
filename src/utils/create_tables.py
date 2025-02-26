c1c7dbf0335194d509b27311edfb1f7939393faf
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-02-26 22:32:25 +0100

    Add create book table query

--- src/utils/create_tables.py
+++ src/utils/create_tables.py
@@ -18,12 +18,26 @@ if __name__ == "__main__":
             DROP TABLE IF EXISTS student;
             CREATE TABLE student (
                 student_id SERIAL PRIMARY KEY,
-                name VARCHAR(128),
-                surname VARCHAR(128),
+                name VARCHAR(128) NOT NULL,
+                surname VARCHAR(128) NOT NULL,
                 grade INT
             );
             """
         )
         cur.execute(create_student_table_query)
+    
+        create_book_table_query=sql.SQL(
+            """
+            DROP TABLE IF EXISTS book;
+            CREATE TABLE book (
+                book_id SERIAL PRIMARY KEY,
+                title VARCHAR(128),
+                author_id INT,
+                copies_available INT DEFAULT 1
+            );
+            """
+        )
+        cur.execute(create_book_table_query)
+
 
     conn.close()
