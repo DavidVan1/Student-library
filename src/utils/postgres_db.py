0e17ba9288678157edea66fe22e1830959dc7f90
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-02-27 22:14:18 +0100

    Add function to get data

--- src/utils/postgres_db.py
+++ src/utils/postgres_db.py
@@ -27,7 +27,7 @@ class PostgresDB:
                 self.connection = None
 
     def insert_data(self, table_name, data_dict):
-        if self.connection is None or self.connection.closed():
+        if self.connection is None or self.connection.closed:
             self.connect()
         
         with self.connection, self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
@@ -48,6 +48,47 @@ class PostgresDB:
             except Exception as e:
                 traceback.print_exc()
                 print(f"Error {e}")
-                
+
             self.connection.close()
             return insert
+    
+
+    def get_data_simple_condition(self, table_name, columns=[], condition_column=None, condition_value=None):
+        result = []
+        if columns == []: columns = ['*']
+        
+        if self.connection is None or self.connection.closed:
+            self.connect()
+        
+        with self.connection, self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            try:
+                if condition_column is None:
+                    select_query=sql.SQL(
+                        """
+                        SELECT {}
+                        FROM {}
+                        """
+                    ).format(
+                        sql.SQL(',').join(map(sql.Identifier, columns)),
+                        sql.Identifier(table_name)
+                    )
+                else:
+                    select_query=sql.SQL(
+                        """
+                        SELECT {}
+                        FROM {}
+                        WHERE {} = {}
+                        """
+                    ).format(
+                        sql.SQL(',').join(map(sql.Identifier, columns)),
+                        sql.Identifier(table_name),
+                        sql.Identifier(condition_column),
+                        sql.Literal(condition_value)
+                    )
+                    cur.execute()
+                    result=cur.fetchall()
+            except Exception as e:
+                traceback.print_exc()
+                print(f"Error {e}")
+        self.connection.close()
+        return result
