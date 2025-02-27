8d2a75fe037870624221afc0a21dc7225733e3c2
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-02-27 21:38:51 +0100

    Add insert_data function

--- src/utils/postgres_db.py
+++ src/utils/postgres_db.py
@@ -25,3 +25,29 @@ class PostgresDB:
             except Exception as e:
                 print(f"Error connecting to database: {e}")
                 self.connection = None
+
+    def insert_data(self, table_name, data_dict):
+        if self.connection is None or self.connection.closed():
+            self.connect()
+        
+        with self.connection, self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            try:
+                insert_query=sql.SQL(
+                    """
+                    INSERT INTO {} ({})
+                    VALUES ({})
+                    RETURNING *;
+                    """
+                ).format(
+                    sql.Identifier(table_name),
+                    sql.SQL(',').join(map(sql.Identifier, data_dict.keys())),
+                    sql.SQL(',').join(map(sql.Literal, data_dict.values()))
+                )
+                cur.execute(insert_query)
+                insert = cur.fetchone()
+            except Exception as e:
+                traceback.print_exc()
+                print(f"Error {e}")
+                
+            self.connection.close()
+            return insert
