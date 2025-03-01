c32a5a6b730643d809953b6ec0f346f24d45592d
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-03-01 22:13:51 +0100

    Add get_students table

--- src/InformationSystem.py
+++ src/InformationSystem.py
@@ -5,3 +5,6 @@ class InformationSystem:
     def __init__(self, db: PostgresDB):
         self.db = db
     
+    def get_students(self):
+        students=self.db.get_all_data(consts.STUDENT_TABLE)
+        return students
