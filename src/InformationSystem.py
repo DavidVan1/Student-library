cd5df616419a0769038742862085406c94f46bfc
Tuan Dávid Nguyen Van <xnguyenv@fi.muni.cz>
2025-03-01 22:16:07 +0100

    Add insert_student function

--- src/InformationSystem.py
+++ src/InformationSystem.py
@@ -8,3 +8,8 @@ class InformationSystem:
     def get_students(self):
         students=self.db.get_all_data(consts.STUDENT_TABLE)
         return students
+
+    def insert_student(self, data_dict):
+        student=self.db.insert_data(consts.STUDENT_TABLE, data_dict)
+        return student
+    
